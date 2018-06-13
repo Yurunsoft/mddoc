ea1f6035dfeac578b65b4bb5f93b59f62d6a8ddf
宇润 <admin@yurunsoft.com>
2018-06-13 10:17:40 +0800

    调整github webhook处理

--- github-webhook.php
+++ github-webhook.php
@@ -1,56 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Yurun\MdDoc\File;
-use Yurun\MdDoc\Builder;
-use Yurun\Util\HttpRequest;
+use Yurun\MdDoc\GithubWebhook;
 
 $config = include __DIR__ . '/config/github.php';
 
-// 密码验证
-if(!isset($_GET['password'], $config['password']) || $config['password'] !== $_GET['password'] || '' === $config['password'])
-{
-    exit('are you ok?');
-}
-
-// post数据
-$data = json_decode(file_get_contents('php://input'), true);
-
-// 事件验证
-if(!isset($_SERVER['HTTP_X_GITHUB_EVENT']) || !in_array($_SERVER['HTTP_X_GITHUB_EVENT'], $config['events']))
-{
-    exit('are you ok?');
-}
-
-// 下载
-
-if(is_dir($config['downloadSavePath']))
-{
-    File::clearDir($config['downloadSavePath']);
-}
-else
-{
-    mkdir($config['downloadSavePath'], 0777, true);
-}
-$zipFileName = File::path($config['downloadSavePath'], 'download.zip');
-$http = new HttpRequest;
-$http->download($zipFileName, $config['downloadUrl']);
-
-// 解压
-
-$zip = new ZipArchive;
-$return = $zip->open($zipFileName);
-if(true !== $return)
-{
-    exit($zipFileName . ' 文件打开失败');
-}
-if(!$zip->extractTo($config['downloadSavePath']))
-{
-    exit($zipFileName . ' 文件解压失败');
-}
-$zip->close();
-
-// 生成
-
-$builder = new Builder($config['downloadSavePath'], $config['htmlSavePath']);
-$builder->build();
+$webhook = new GithubWebhook($config);
+$webhook->handle();
---
+++ src/GithubWebhook.php
@@ -0,0 +1,71 @@
+<?php
+namespace Yurun\MdDoc;
+
+use Yurun\MdDoc\File;
+use Yurun\MdDoc\Builder;
+use Yurun\Util\HttpRequest;
+
+class GithubWebhook
+{
+	/**
+	 * 配置
+	 * @var array
+	 */
+	protected $config;
+
+	public function __construct($config)
+	{
+		$this->config = $config;
+	}
+
+	public function handle()
+	{
+		// 密码验证
+		if(!isset($_GET['password'], $this->config['password']) || $this->config['password'] !== $_GET['password'] || '' === $this->config['password'])
+		{
+			exit('are you ok?');
+		}
+
+		// post数据
+		$data = json_decode(file_get_contents('php://input'), true);
+
+		// 事件验证
+		if(!isset($_SERVER['HTTP_X_GITHUB_EVENT']) || !in_array($_SERVER['HTTP_X_GITHUB_EVENT'], $this->config['events']))
+		{
+			exit('are you ok?');
+		}
+
+		// 下载
+
+		if(is_dir($this->config['downloadSavePath']))
+		{
+			File::clearDir($this->config['downloadSavePath']);
+		}
+		else
+		{
+			mkdir($this->config['downloadSavePath'], 0777, true);
+		}
+		$zipFileName = File::path($this->config['downloadSavePath'], 'download.zip');
+		$http = new HttpRequest;
+		$http->download($zipFileName, $this->config['downloadUrl']);
+
+		// 解压
+
+		$zip = new \ZipArchive;
+		$return = $zip->open($zipFileName);
+		if(true !== $return)
+		{
+			exit($zipFileName . ' 文件打开失败');
+		}
+		if(!$zip->extractTo($this->config['downloadSavePath']))
+		{
+			exit($zipFileName . ' 文件解压失败');
+		}
+		$zip->close();
+
+		// 生成
+
+		$builder = new Builder($this->config['downloadSavePath'], $this->config['htmlSavePath']);
+		$builder->build();
+	}
+}
